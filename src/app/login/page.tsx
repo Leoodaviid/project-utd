c7b224581add6b4883e9d56e44d9f09f44f6f47a
Leoodaviid <david_ax7@hotmail.com>
2023-05-13 15:59:20 -0300

    feat: create page auth and component input

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Login() {
-  return (
-    <div className="relative h-full w-full">
-      <div className="bg-black w-full h-full  lg:bg-opacity-50">nav</div>
-    </div>
-  );
-}
