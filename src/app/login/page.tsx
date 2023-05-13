274c050f98bc93088a13278bafcf4b380074796e
Leoodaviid <david_ax7@hotmail.com>
2023-05-13 13:27:25 -0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="relative h-full w-full">
+      <div className="bg-black w-full h-full  lg:bg-opacity-50">nav</div>
+    </div>
+  );
+}
